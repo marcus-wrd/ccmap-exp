7378970e005a24f2285a6f197c85ee97fb37fc01
marcus-wrd <123615006+marcus-wrd@users.noreply.github.com>
2023-12-23 22:48:03 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -437,7 +437,7 @@ async function saveToFile() {
     input.onchange = async function e() {
         const file = e.target.files[0];
         const reader = new FileReader();
-        reader.onload = function(event) {
+        reader.onload = async function(event) {
             try {
                 const data = JSON.parse(event.target.result);
                 cy.json(data); // Load the data directly
