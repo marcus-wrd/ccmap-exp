319c35ad9edbfd291f79fc646ceaec9cab7a101e
marcus-wrd <123615006+marcus-wrd@users.noreply.github.com>
2023-12-23 22:47:01 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -434,7 +434,7 @@ async function saveToFile() {
    async function loadFromFile() {
     const input = document.createElement('input');
     input.type = 'file';
-    input.onchange = e => {
+    input.onchange = async function e() {
         const file = e.target.files[0];
         const reader = new FileReader();
         reader.onload = function(event) {
