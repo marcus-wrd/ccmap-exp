71b2b3e34a6562d71f2a44925e5718afe120f58f
marcus-wrd <123615006+marcus-wrd@users.noreply.github.com>
2024-01-08 10:50:46 +0800

    Update hoverad.js

--- static/hoverad.js
+++ static/hoverad.js
@@ -1,5 +1,9 @@
 function off() {
      document.getElementById("overlay").style.display = "none";
 }
+function off2() {
+     document.getElementById("overlay2").style.display = "none";
+}
+off2();
 off();
  
